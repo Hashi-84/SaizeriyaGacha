b74b2ffc2c7909bbd34f2d4eba281c9f2793fdca
O-Ryosuke <ryousuke0084@gmail.com>
2024-07-05 21:40:57 +0900

    大まかに5か所を修正。 ・画面の向きを変えたりダークモードの切り替え等でアクティビティが破棄されても画面上の入力内容を保持するように実装を変更 ・アプリアイコンを独自のものに差し替え ・ガチャボタンにアイコンを追加 ・オプション項目をBottomSheetに移設、呼び出し用のFABを追加 ・リストのUIを調整 　- 注文番号を常に左端に表示するよう変更 　- 1行ごとの高さをやや高めに変更

--- app/src/main/java/jp/hashi/menuarranger/MainActivity.kt
+++ app/src/main/java/jp/hashi/menuarranger/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package jp.hashi.menuarranger
 
 import android.os.Bundle
@@ -6,25 +8,41 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,6 +55,8 @@ import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModelProvider
 import jp.hashi.menuarranger.model.MenuItem
 import jp.hashi.menuarranger.ui.theme.MenuArrangerTheme
+import kotlinx.coroutines.launch
+import kotlin.reflect.KFunction2
 
 class MainActivity : ComponentActivity() {
     private lateinit var viewModel: MenuViewModel
@@ -52,9 +72,7 @@ class MainActivity : ComponentActivity() {
                 Surface {
                     Greeting(
                         viewModel,
-                        onChooseBtnClick = { viewModel.chooseMenuItems(it) },
-                        onAlcoholSwitchClick = { viewModel.setAlcohol(it) },
-                        onKidsSwitchClick = { viewModel.setKids(it) }
+                        onChooseBtnClick = (viewModel::onChooseBtnClick),
                     )
                 }
             }
@@ -65,91 +83,163 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(
     viewModel: MenuViewModel,
-    onChooseBtnClick: (Int) -> Unit,
-    onAlcoholSwitchClick: (Boolean) -> Unit,
-    onKidsSwitchClick: (Boolean) -> Unit
+    onChooseBtnClick: KFunction2<Int, (String) -> Unit, Unit>
 ) {
     val menuList = viewModel.chosenMenuItems.observeAsState()
-    var ceilingPrice by remember { mutableIntStateOf(1000) }
-    var alcoholContains by remember { mutableStateOf(true) }
-    var kidsContains by remember { mutableStateOf(true) }
-    var showMenuId by remember { mutableStateOf(false) }
 
-    Column(Modifier.fillMaxSize()) {
-        Column(verticalArrangement = Arrangement.Bottom, modifier = Modifier
-            .wrapContentHeight()
-            .padding(bottom = 8.dp)) {
-            Row(Modifier.padding(8.dp)) {
-                TextField(
-                    value = ceilingPrice.toString(),
-                    onValueChange = { ceilingPrice = try {it.toInt()} catch (e: NumberFormatException) {0}},
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.weight(2f),
-                    label = { Text(text = "上限金額") }
-                )
-                Text("円",
-                    Modifier
-                        .align(Alignment.Bottom)
-                        .padding(8.dp))
-                Button(
-                    onClick = { onChooseBtnClick(ceilingPrice) },
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .align(Alignment.CenterVertically)
-                        .padding(start = 8.dp)
-                ) {
-                    Text(text = "ガチャ！")
-                }
-            }
-            Column(Modifier.padding(start = 16.dp)) {
-                Row {
-                    Switch(onCheckedChange = {
-                        alcoholContains = it
-                        onAlcoholSwitchClick(it)
-                    }, checked = alcoholContains)
-                    Text(text = stringResource(R.string.contains_alcohol),
-                        Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(start = 4.dp, end = 8.dp))
-                }
-                Row {
-                    Switch(onCheckedChange = {
-                        kidsContains = it
-                        onKidsSwitchClick(it)
-                    }, checked = kidsContains)
-                    Text(text = stringResource(R.string.contains_kids),
-                        Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(start = 4.dp, end = 8.dp))
-                }
-                Row {
-                    Switch(onCheckedChange = {
-                        showMenuId = it
-                    }, checked = showMenuId)
-                    Text(text = stringResource(R.string.show_menu_number),
-                        Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(start = 4.dp, end = 8.dp))
-                }
-            }
-            Text("合計: " + menuList.value?.sumOf { it.price }.toString() + "円",
-                fontSize = androidx.compose.material3.MaterialTheme.typography.displaySmall.fontSize,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(start = 16.dp)
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val hostState = remember { SnackbarHostState() }
+
+    val alcoholContains by viewModel.alcoholContains.observeAsState(true)
+    val kidsContains by viewModel.kidsContains.observeAsState(true)
+    val ceilingPrice by viewModel.ceilingPrice.observeAsState(1000)
+    val showMenuId by viewModel.showMenuId.observeAsState(true)
+
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text(text = "条件を変更") },
+                icon = {
+                    Icon(
+                        Icons.Filled.Edit,
+                        contentDescription = "Change conditions to choose menus."
+                    )
+                },
+                onClick = { showBottomSheet = true }
             )
+        },
+        snackbarHost = { SnackbarHost(hostState) }
+    ) { innerPadding ->
+        Column(Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)) {
+            Column(
+                verticalArrangement = Arrangement.Bottom,
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .padding(paddingValues = innerPadding)
+            ) {
+                Row(Modifier.padding(8.dp)) {
+                    TextField(
+                        value = ceilingPrice.toString(),
+                        onValueChange = {
+                            viewModel.setCeilingPrice(
+                                try {
+                                    it.toInt()
+                                } catch (e: NumberFormatException) {
+                                    0
+                                }
+                            )
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier.weight(2f),
+                        label = { Text(text = "上限金額") }
+                    )
+                    Text(
+                        "円",
+                        Modifier
+                            .align(Alignment.Bottom)
+                            .padding(8.dp)
+                    )
+                    Button(
+                        onClick = {
+                            onChooseBtnClick(ceilingPrice) {
+                                scope.launch {
+                                    hostState.showSnackbar(it)
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 8.dp)
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                Icons.Filled.Refresh,
+                                contentDescription = "Shuffle and choose menus.",
+                                Modifier.padding(end = 4.dp)
+                            )
+                            Text(
+                                stringResource(R.string.gatcha),
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                }
+                Row(verticalAlignment = Alignment.Bottom) {
+                    Text(
+                        "合計: ${menuList.value?.sumOf { it.price }.toString()}円",
+                        fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                    Text(
+                        "(${menuList.value?.size}品)",
+                        Modifier.padding(bottom = 5.dp, start = 4.dp)
+                    )
+                }
+            }
+            HorizontalDivider(Modifier.padding(top = 8.dp), thickness = 1.dp)
+            LazyColumn(
+                verticalArrangement = Arrangement.Top, modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(horizontal = 8.dp)
+            ) {
+                menuList.value?.size?.let {
+                    items(it) { item ->
+                        (if (showMenuId) 0.dp else 8.dp).let { padding ->
+                            Column(Modifier.padding(vertical = padding)) {
+                                MenuListItem(menuList.value!![item], showMenuId)
+                                HorizontalDivider(
+                                    thickness = 1.dp,
+                                    modifier = Modifier.padding(top = padding)
+                                )
+                            }
+                        }
+                    }
+                }
+                item { Spacer(Modifier.height(74.dp)) }
+            }
         }
-        LazyColumn(verticalArrangement = Arrangement.Top, modifier = Modifier
-            .fillMaxHeight()
-            .padding(horizontal = 8.dp)) {
-            menuList.value?.size?.let {
-                items(it) { item ->
-                    Column {
-                        MenuListItem(menuList.value!![item], showMenuId)
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                sheetState = sheetState
+            ) {
+                Column(
+                    Modifier
+                        .padding(16.dp)
+                        .padding(bottom = 36.dp)
+                ) {
+
+                    Row {
+                        Switch(
+                            onCheckedChange = viewModel::setAlcoholContains,
+                            checked = alcoholContains
+                        )
+                        Text(
+                            text = stringResource(R.string.contains_alcohol),
+                            Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 4.dp, end = 8.dp)
+                        )
+                    }
+                    Row {
+                        Switch(
+                            onCheckedChange =
+                            viewModel::setKidsContains, checked = kidsContains
+                        )
+                        Text(
+                            text = stringResource(R.string.contains_kids),
+                            Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 4.dp, end = 8.dp)
+                        )
                     }
                 }
             }
         }
-
     }
 }
 
@@ -158,19 +248,22 @@ fun MenuListItem(item: MenuItem, showMenuId: Boolean) {
     Row(
         Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp)
+            .padding(vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
+        if (showMenuId) FilledTonalButton(onClick = {}, Modifier.padding(end = 8.dp)) {
+            Text(
+                text = item.menuId.toString(),
+                style = androidx.compose.material3.MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        }
         Text(
             item.name, Modifier.weight(1f),
             style = androidx.compose.material3.MaterialTheme.typography.bodyLarge
         )
-        if (showMenuId) Text(
-            text = "ID: " + item.menuId.toString(),
-            style = androidx.compose.material3.MaterialTheme.typography.bodyMedium,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(Alignment.CenterVertically)
-        )
-        else Text(
+        Text(
             text = item.price.toString() + "円",
             style = androidx.compose.material3.MaterialTheme.typography.bodyMedium,
             fontWeight = FontWeight.Bold,
@@ -221,7 +314,7 @@ fun GreetingPreview() {
                     "appetizer",
                     true
                 ),
-            ).forEach { MenuListItem(it, false) }
+            ).forEach { MenuListItem(it, true) }
         }
     }
 }
